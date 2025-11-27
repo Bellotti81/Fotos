644f37e9f2d5a2ec5c23f6961b216a8f92a69100
= <=>
2025-11-26 22:35:36 -0300

    Criei o projeto Fotos - 26/11/2025 - 22:30h

--- ExercicioHttpClient/ViewModels/PostagensViewModel.cs
+++ ExercicioHttpClient/ViewModels/PostagensViewModel.cs
@@ -1,13 +1,16 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using ExercicioHttpClient.Models;
+using ExercicioHttpClient.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace ExercicioHttpClient.ViewModels
 {
-    public class PostagensViewModel : ObservableObject
+    public partial class PostagensViewModel : ObservableObject
     {
         [ObservableProperty]
         public int id;
@@ -17,5 +20,15 @@ namespace ExercicioHttpClient.ViewModels
         public string title;
         [ObservableProperty]
         public string body;
+        public ICommand CarregarPostagenscommand => new Command(async () => CarregaPostagens());
+        
+        public async void CarregaPostagens()
+        {
+            List<Postagem> Listar = new List<Postagem>();
+            Listar = await new PostagensService().GetPostagens();
+            Id = Listar[0].Id;
+            Title = Listar[0].Title;
+            Body = Listar[0].Body;
+        }
     }
 }
